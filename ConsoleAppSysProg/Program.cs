5b1f6b6246479d0ac60a234acaf31b6c2f36611d
Igor Vinokurov <harryvin@bk.ru>
2024-06-25 23:35:01 +0300

    Добавьте файлы проекта.

---
+++ ConsoleAppSysProg/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using static System.Console;
+
+namespace ConsoleAppSysProg
+{
+    partial class Program
+    {
+        [LibraryImport("user32.dll", EntryPoint = "MessageBoxW", StringMarshalling = StringMarshalling.Utf16)]
+        public static partial int MessageBox(IntPtr hWnd, string text, string caption, uint type);
+
+        static void Main()
+        {
+            string userName = GetInput("Введите ваше имя: ");
+            while (!IsValidName(userName))
+            {
+                WriteLine("Ошибка! Имя не должно содержать цифр или быть пустым!");
+                userName = GetInput("Введите ваше имя: ");
+            }
+            string userAge = GetInput("Введите ваш возраст: ");
+            int age;
+            while (!int.TryParse(userAge, out age) || age <= 0)
+            {
+                WriteLine("Ошибка! Введите корректный возраст (целое число больше 0).");
+                userAge = GetInput("Введите ваш возраст: ");
+            }
+            string userOccupation = GetInput("Введите вашу профессию: ");
+            while (!IsValidName(userOccupation))
+            {
+                WriteLine("Ошибка! Профессия не должна содержать цифр или быть пустой!");
+                userOccupation = GetInput("Введите вашу профессию: ");
+            }
+            DisplayMessageBox(userName, "Имя");
+            DisplayMessageBox(age.ToString(), "Возраст");
+            DisplayMessageBox(userOccupation, "Профессия");
+            DisplayMessageBox("Спасибо за использование!", "Вот и всё!");
+        }
+        static string GetInput(string prompt)
+        {
+            Write(prompt);
+            return ReadLine();
+        }
+        static int DisplayMessageBox(string message, string caption)
+        {
+            return MessageBox(IntPtr.Zero, message, caption, 0);
+        }
+        static bool IsValidName(string name)
+        {
+            return Regex.IsMatch(name, @"^[a-zA-Zа-яА-ЯёЁ-]+$");
+        }
+    }
+}
