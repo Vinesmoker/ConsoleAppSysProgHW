f0a2d26bb252197518e233c4a08780acca4c0002
Igor Vinokurov <harryvin@bk.ru>
2024-06-28 21:48:19 +0300

    HW1T3 Done

---
+++ HW1T3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    [DllImport("kernel32.dll")]
+    private static extern bool Beep(uint dwFreq, uint dwDuration);
+    [DllImport("user32.dll")]
+    private static extern bool MessageBeep(uint uType);
+
+    static void Main()
+    {
+        Console.WriteLine("Нажмите '1', '2' или '3' для воспроизведения звука. Нажмите 'Esc' для выхода.");
+
+        ConsoleKey key;
+        do
+        {
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+            key = keyInfo.Key;
+
+            switch (key)
+            {
+                case ConsoleKey.D1:
+                    Beep(1000, 300);
+                    break;
+                case ConsoleKey.D2:
+                    Beep(1500, 300);
+                    break;
+                case ConsoleKey.D3:
+                    MessageBeep(0xFFFFFFFF);
+                    break;
+                case ConsoleKey.Escape:
+                    Console.WriteLine("Программа завершена.");
+                    break;
+            }
+        } while (key != ConsoleKey.Escape);
+    }
+}
